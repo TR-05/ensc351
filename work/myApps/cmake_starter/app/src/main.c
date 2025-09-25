d360e93f4d4eed6e00be39d7e10a512a599ab260
trevor <trevor@debian.local>
2025-09-25 14:10:39 -0700

    as1 app functions but messy

--- work/myApps/cmake_starter/app/src/main.c
+++ work/myApps/cmake_starter/app/src/main.c
@@ -5,9 +5,12 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/time.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
+    srand(time(NULL));
     joystick_initialize();
     led_initialize();
     button_initialize();
@@ -17,6 +20,8 @@ int main()
     printf("Hello embedded world, from Trevor!\n");
     printf("When the LEDs light up, press the joystick in that direction!\n");
     printf("(Press left or right to exit)\n");
+
+    int best_time = 5000;
 
     while (1)
     {
@@ -30,22 +35,25 @@ int main()
             led_pwr_set_on();
             usleep(250000);
         }
+        led_act_set_off();
+        led_pwr_set_off();
         if (fabs(joystick_get_y_normalized()) > 0.1)
         {
             printf("Please let go of joystick\n");
         }
+        usleep(100); // let adc calculate
         while (fabs(joystick_get_y_normalized()) > 0.1)
         {
             usleep(10000);
         }
-        int random_sleep_time = 1000000;
+        int random_sleep_time = 500000 + (rand() % 2500000); // Random between 0.5-2.5 seconds
         usleep(random_sleep_time);
         if (fabs(joystick_get_y_normalized()) > 0.1)
         {
             printf("too soon\n"); // LOOP BACK TO WHILE LOOP
         }
 
-        int dir = 1; // randomly choose up or down
+        int dir = (rand() % 2) ? 1 : -1; // Randomly choose 1 or -1 (up or down)
         if (dir == 1)
         {
             printf("Press UP now!\n");
@@ -60,21 +68,71 @@ int main()
         }
         struct timeval start_tv, current_tv;
         gettimeofday(&start_tv, NULL);
-
-        while (fabs(joystick_get_y_normalized()) < 0.5)
+        double elapsed = 0;
+        while (fabs(joystick_get_y_normalized()) < 0.75)
         {
+            usleep(500); // let adc calculate
+            if (fabs(joystick_get_x_normalized()) > 0.75)
+            {
+                break;
+            }
+
             gettimeofday(&current_tv, NULL);
-            double elapsed = (current_tv.tv_sec - start_tv.tv_sec) +
-                             (current_tv.tv_usec - start_tv.tv_usec) / 1000000.0;
+            elapsed = (current_tv.tv_sec - start_tv.tv_sec) +
+                      (current_tv.tv_usec - start_tv.tv_usec) / 1000000.0;
             if (elapsed >= 5.0)
-                break; // 5 second timeout
-            usleep(10000);
+                break;   // 5 second timeout
+            usleep(500); // let adc calculate
         }
-        // float x = joystick_get_y_normalized();
+        if (elapsed >= 5.0)
+        {
+            printf("No input within 5000ms; quitting!\n");
+            break; // 5 second timeout
+        }
+        led_act_set_off();
+        led_pwr_set_off();
+        usleep(500); // let adc calculate
+        float y = joystick_get_y_normalized();
+        usleep(500); // let adc calculate
+        float x = joystick_get_x_normalized();
+        // printf("x: %.2f, y:  %.2f\n", x, y);
+        if (fabs(x) > 0.75)
+        {
+            printf("User selected to quit.\n");
+            break;
+        }
+
         gettimeofday(&current_tv, NULL);
-        double reaction_time = (current_tv.tv_sec - start_tv.tv_sec) +
-                               (current_tv.tv_usec - start_tv.tv_usec) / 1000000.0;
-        printf("Your reaction time was %dms\n", (int)(reaction_time * 1000));
+        int reaction_time = (int)(1000 * ((current_tv.tv_sec - start_tv.tv_sec) +
+                                          (current_tv.tv_usec - start_tv.tv_usec) / 1000000.0));
+        if (y / fabs(y) == dir)
+        {
+            printf("Correct!\n");
+            if (reaction_time < best_time)
+            {
+                best_time = reaction_time;
+                printf("New best time!\n");
+            }
+            printf("Your reaction time was %dms; best so far in game is %dms\n", reaction_time, best_time);
+            for (int i = 0; i < 5; i++)
+            {
+                led_act_set_on();
+                usleep(100000);
+                led_act_set_off();
+                usleep(100000);
+            }
+        }
+        else
+        {
+            printf("Incorrect.\n");
+            for (int i = 0; i < 5; i++)
+            {
+                led_pwr_set_on();
+                usleep(100000);
+                led_pwr_set_off();
+                usleep(100000);
+            }
+        }
     }
 
     joystick_disable(); // idk if this matters
